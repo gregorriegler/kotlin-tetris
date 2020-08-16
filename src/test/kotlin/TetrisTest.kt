05b0523bd0f32ab2861e5df194c4543fc6a29d55
Gregor Riegler <gregor.riegler@ebcont.com>
2020-08-16 18:05:54 +0200

    t nested test to prepare for two column tetris

--- src/test/kotlin/TetrisTest.kt
+++ src/test/kotlin/TetrisTest.kt
@@ -1,111 +1,120 @@
 import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 
-class TetrisTest {
+class `A Tetris` {
 
-    private var tetris = Tetris(1, 3)
+    @Nested
+    inner class `with a single column should` {
+        private var tetris = Tetris(1, 3)
 
-    @Test
-    fun `starts empty`() {
-        assertDisplays(
-            tetris,
-            """
-            _
-            _
-            _
-            """,
-            0
-        )
+        @Test
+        fun `start empty`() {
+            assertDisplays(
+                tetris,
+                """
+                _
+                _
+                _
+                """,
+                0
+            )
+        }
+
+        @Test
+        fun `drop a stone from the top`() {
+            tetris.tick()
+
+            assertDisplays(
+                tetris,
+                """
+                #
+                _
+                _
+                """,
+                0
+            )
+        }
+
+        @Test
+        fun `continue dropping a stone`() {
+            repeat(2) { tetris.tick() }
+
+            assertDisplays(
+                tetris,
+                """
+                _
+                #
+                _
+                """,
+                0
+            )
+        }
+
+        @Test
+        fun `land stone on the bottom`() {
+            repeat(3) { tetris.tick() }
+
+            assertDisplays(
+                tetris,
+                """
+                _
+                _
+                #
+                """,
+                0
+            )
+        }
+
+        @Test
+        fun `dissolve stone from bottom as score increases`() {
+            repeat(4) { tetris.tick() }
+
+            assertDisplays(
+                tetris,
+                """
+                _
+                _
+                _
+                """,
+                1
+            )
+        }
+
+        @Test
+        fun `drop another stone`() {
+            repeat(5) { tetris.tick() }
+
+            assertDisplays(
+                tetris,
+                """
+                #
+                _
+                _
+                """,
+                1
+            )
+        }
+
+        @Test
+        fun `score another stone`() {
+            repeat(8) { tetris.tick() }
+
+            assertDisplays(
+                tetris,
+                """
+                _
+                _
+                _
+                """,
+                2
+            )
+        }
     }
 
-    @Test
-    fun `dropped stone starts at top`() {
-        tetris.tick()
-
-        assertDisplays(
-            tetris,
-            """
-            #
-            _
-            _
-            """,
-            0
-        )
-    }
-
-    @Test
-    fun `dropped stone falls`() {
-        repeat(2) { tetris.tick() }
-
-        assertDisplays(
-            tetris,
-            """
-            _
-            #
-            _
-            """,
-            0
-        )
-    }
-
-    @Test
-    fun `dropped stone falls to bottom`() {
-        repeat(3) { tetris.tick() }
-
-        assertDisplays(
-            tetris,
-            """
-            _
-            _
-            #
-            """,
-            0
-        )
-    }
-
-    @Test
-    fun `dropped stone dissolves from bottom as score increases`() {
-        repeat(4) { tetris.tick() }
-
-        assertDisplays(
-            tetris,
-            """
-            _
-            _
-            _
-            """,
-            1
-        )
-    }
-
-    @Test
-    fun `drops another stone`() {
-        repeat(5) { tetris.tick() }
-
-        assertDisplays(
-            tetris,
-            """
-            #
-            _
-            _
-            """,
-            1
-        )
-    }
-
-    @Test
-    fun `scores another stone`() {
-        repeat(8) { tetris.tick() }
-
-        assertDisplays(
-            tetris,
-            """
-            _
-            _
-            _
-            """,
-            2
-        )
+    @Nested
+    inner class `with two columns should` {
+        private var tetris = Tetris(2, 3)
     }
 
     private fun assertDisplays(tetris: Tetris, ouput: String, score: Int) {
